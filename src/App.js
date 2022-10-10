701c027fe4eec4883657d23634f6b5717fcc91c8
tiagodlb <tigolimab223@gmail.com>
2022-10-10 17:51:59 -0300

    :wrench: chore: organize the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy, Component, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import UserContext from "./context/userContext";
+
+
+const Loading = () => (
+  <div>Loading...</div>
+);
+
+const LazyWrapper = (Component) => (props) => (
+  <Suspense fallback={<Loading />}>
+    <Component {...props} />
+  </Suspense>
+)
+
+const Login = LazyWrapper(lazy(() => import("./pages/Login/login")));
+const Register = LazyWrapper(lazy(() => import("./pages/Register/register")));
+const Home = LazyWrapper(lazy(() => import("./pages/Home/home")));
+const CreateSurvey = LazyWrapper(lazy(() => import("./pages/Surveys/createSurvey")));
+// const Survey = LazyWrapper(lazy(() => import("./pages/Timeline/Random")));
 
 function App() {
+  const [user, setUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContext.Provider value={{ user, setUser }}>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+             <Route path="/home" element={<Home />} />
+            {/* <Route path="/survey" element={<Survey />} /> */}
+            <Route path="/create/survey" element={<CreateSurvey />} /> 
+            <Route path="*" element={<div>Not found!</div>} />
+        </Routes>
+      </Router>
+    </UserContext.Provider>
+
+  )
 }
 
 export default App;
